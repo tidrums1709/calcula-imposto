bf1812a6a2e4073262cfbd17652130bff9dfd512
tidruns@gmail.com <tidruns@gmail.com>
2021-04-30 15:34:13 -0300

    First commit

---
+++ src/main/java/br/com/tiago/calculaimposto/CalculaImpostoApplication.java
@@ -0,0 +1,13 @@
+package br.com.tiago.calculaimposto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculaImpostoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculaImpostoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tiago/calculaimposto/CalculaImpostoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tiago.calculaimposto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculaImpostoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
